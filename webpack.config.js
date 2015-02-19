51802dec46d81c7d45d114c90998af829daab5e3
Shun Hotta <stars.big2men@gmail.com>
2015-02-19 11:33:56 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-module.exports = {
+var webpack = require('webpack');
+var production = process.env.NODE_ENV == 'production'
+var outputPrefix = 'qblog-category-sort';
+
+var options = {
   entry: './index.js',
   output: {
     path: '.',
-    filename: 'qblog-category-sort.js'
+    filename: outputPrefix + '.js'
   },
   externals: {
     "jquery": "jQuery"
   }
 }
+
+if (production) {
+  options.output.filename = outputPrefix + '.min.js'
+  options.plugins = [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
+
+module.exports = options;
